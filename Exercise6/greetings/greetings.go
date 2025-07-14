efd2368d7b0d27e6004815644e78c3a98ba97f2d
JamestarKurbah <jamestarkurbah@gmail.com>
2025-07-15 00:21:59 +0530

    writing test for greetings

---
+++ Exercise6/greetings/greetings.go
@@ -0,0 +1,43 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	//message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat()) // test fails
+	return message, nil
+}
+
+// Hello returns a map that associates each of the named person.
+// with greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to accociated names with message
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// in map, associate the retrieved message with the name
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v Welcome!",
+		"Great to see you,%v",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # GO
-
---Create a Go module
---Call your code from another module
---Return and handle an error
---Return a random greeting
---Return greetings for multiple people
+```
+--Exercise 1 Create a Go module
+--Exercise 2 Call your code from another module
+--Exercise 3 Return and handle an error
+--Exercise 4 Return a random greeting
+--Exercise 5 Return greetings for multiple people
+--Exercise 6 Add a test
+```
